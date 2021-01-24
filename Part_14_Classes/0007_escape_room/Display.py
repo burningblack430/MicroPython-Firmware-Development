7cd753a80eb93be2f01ee034de3ecf4c1265f3cb
Kevin Thomas <kevin@mytechnotalent.com>
2021-01-24 07:10:20 -0500

    Update Part 14

--- Part_14_Classes/0007_escape_room/Display.py
+++ Part_14_Classes/0007_escape_room/Display.py
@@ -11,14 +11,41 @@ class Display:
 
         Params:
             oled: object
-            text: str
+            question: str
         """
         text_line_1 = text[0:15]
         text_line_2 = text[15:30]
         text_line_3 = text[30:45]
         text_line_4 = text[45:]
-
         oled.text(text_line_1, 0, 0)
         oled.text(text_line_2, 0, 10)
         oled.text(text_line_3, 0, 20)
         oled.text(text_line_4, 0, 30)
+
+    @staticmethod
+    def texts(oled, text_1, text_2, text_3=None, text_4=None):
+        """
+        Method to take a 4 text strings and break it up into
+        a max of 4 lines to properly display on the oled
+
+        Params:
+            oled: object
+            text_1: str
+            text_2: str
+            text_3: str, optional
+            text_4: str, optional
+        """
+        text_line_3 = None
+        text_line_4 = None
+        text_line_1 = text_1[0:15]
+        text_line_2 = text_2[15:30]
+        if text_3:
+            text_line_3 = text_3[30:45]
+        if text_4:
+            text_line_4 = text_4[45:]
+        oled.text(text_line_1, 0, 0)
+        oled.text(text_line_2, 0, 10)
+        if text_3:
+            oled.text(text_line_3, 0, 20)
+        if text_4:
+            oled.text(text_line_4, 0, 30)
--- Part_14_Classes/0007_escape_room/main.py
+++ Part_14_Classes/0007_escape_room/main.py
@@ -1,6 +1,9 @@
+# esptool.py --chip esp32 --port COM3 erase_flash
+# esptool.py --chip esp32 --port COM3 --baud 460800 write_flash -z 0x1000 *.bin
 # python pyboard.py --device COM3 -f cp main.py :
 
-from machine import Pin, I2C
+from time import sleep
+from machine import Pin, SoftI2C
 import neopixel
 import ssd1306
 from Grid import Grid
@@ -13,12 +16,12 @@ LED_COUNT = 64
 GRID_WIDTH = 8
 GRID_HEIGHT = 8
 np = neopixel.NeoPixel(Pin(2), LED_COUNT)
-i2c = I2C(-1, scl=Pin(22), sda=Pin(21))
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
-btn_1 = Pin(33, Pin.IN)
-btn_2 = Pin(32, Pin.IN)
-btn_3 = Pin(35, Pin.IN)
-btn_4 = Pin(34, Pin.IN)
+btn_1 = Pin(13, Pin.IN)
+btn_2 = Pin(12, Pin.IN)
+btn_3 = Pin(14, Pin.IN)
+btn_4 = Pin(27, Pin.IN)
 grid = Grid(GRID_WIDTH, GRID_HEIGHT)
 player = EscapeRoomPlayer()
 file_manager = FileManager()
@@ -41,52 +44,59 @@ if __name__ == '__main__':
         previous_player_location = player_location
         grid.display(np, GRID_HEIGHT, GRID_WIDTH, LED_COUNT, update_grid)
 
-        while True:
-            # noinspection PyArgumentList
-            if btn_1.value():
-                player_location = player.move_east(grid)
-                update_grid = grid.update(player)
-                break
-            elif btn_2.value():
-                player_location = player.move_west(grid)
-                update_grid = grid.update(player)
-                break
-            elif btn_3.value():
-                player_location = player.move_north(grid)
-                update_grid = grid.update(player)
-                break
-            elif btn_4.value():
-                player_location = player.move_south(grid)
-                update_grid = grid.update(player)
-                break
+        print(btn_1.value())
+        print(btn_2.value())
+        print(btn_3.value())
+        print(btn_4.value())
 
-        if generate_random_location:
-            random_location = (x, y) = game.generate_random_numbers(grid)
-            print(random_location)
-            generate_random_location = False
-        if random_location == player_location and random_location != previous_player_location:
-            random_question, answer_1, answer_2, answer_3, correct_answer_index, correct_answer \
-                = game.ask_random_question()
-            display.text(oled, random_question)
-            oled.show()
-            generate_random_location = True
-        #     say('Press 1 for {0}.'.format(answer_1), speed=SPEED)
-        #     say('Press 2 for {0}.'.format(answer_2), speed=SPEED)
-        #     say('Press 3 for {0}.'.format(answer_3), speed=SPEED)
-
+        input()
+        # while True:
+        #     # noinspection PyArgumentList
+        #     if btn_1.value():
+        #         player_location = player.move_east(grid)
+        #         update_grid = grid.update(player)
+        #         break
+        #     elif btn_2.value():
+        #         player_location = player.move_west(grid)
+        #         update_grid = grid.update(player)
+        #         break
+        #     elif btn_3.value():
+        #         player_location = player.move_north(grid)
+        #         update_grid = grid.update(player)
+        #         break
+        #     elif btn_4.value():
+        #         player_location = player.move_south(grid)
+        #         update_grid = grid.update(player)
+        #         break
+        #
+        # if generate_random_location:
+        #     random_location = (x, y) = game.generate_random_numbers(grid)
+        #     print(random_location)
+        #     generate_random_location = False
+        #
+        # if random_location == player_location and random_location != previous_player_location:
+        #     random_question, answer_1, answer_2, answer_3, correct_answer_index, correct_answer \
+        #         = game.ask_random_question()
+        #     display.text(oled, random_question)
+        #     oled.show()
+        #     sleep(5)
+        #     oled.fill(0)
+        #     oled.show()
+        #     display.texts(oled, answer_1, answer_2, answer_3)
+        #     oled.show()
         #     while True:
-        #         if button_a.is_pressed():
+        #         # noinspection PyArgumentList
+        #         if btn_1.value():
         #             response = 1
         #             break
-        #         elif pin_logo.is_touched():
+        #         elif btn_2.value():
         #             response = 2
         #             break
-        #         elif button_b.is_pressed():
+        #         elif btn_3.value():
         #             response = 3
         #             break
         #     if response == correct_answer_index + 1:
-        #         display.show(Image.SURPRISED)
-        #         say(game.correct_answer_response(), speed=SPEED)
+        #         display.text(oled, game.correct_answer_response())
         #         inventory = player.get_inventory(file_manager)
         #         player.inventory.append(inventory)
         #         if 'Red Key' in player.inventory:
@@ -94,17 +104,14 @@ if __name__ == '__main__':
         #         if 'Red Key' not in player.inventory and not final_question:
         #             receive_red_key = game.generate_random_number(grid)
         #             if receive_red_key == 2:
-
-        #                 say(player.pick_up_red_key(file_manager), speed=SPEED)
+        #                 display.text(oled, player.pick_up_red_key(file_manager))
         #                 final_question = True
         #             else:
-
-        #                 say(player.without_red_key(), speed=SPEED)
+        #                 display.text(oled, player.without_red_key())
         #         elif final_question:
-
-        #             say(game.win(file_manager), speed=SPEED)
-        #             music.play(music.POWER_UP)
-        #             display.show(Image.ALL_CLOCKS, loop=True, delay=100)
+        #             display.text(oled, game.win(file_manager))
+        #             # music.play(music.POWER_UP)
+        #             # display.show(Image.ALL_CLOCKS, loop=True, delay=100)
         #     else:
-        #         display.show(Image.SURPRISED)
-        #         say(game.incorrect_answer_response(correct_answer), speed=SPEED)
+        #         display.text(oled, game.incorrect_answer_response(correct_answer))
+        #     generate_random_location = True
