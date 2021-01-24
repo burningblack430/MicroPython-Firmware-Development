f3984846a6d1e0fac54d8bc0966f3f1ec3b906d8
Kevin Thomas <kevin@mytechnotalent.com>
2021-01-24 07:18:09 -0500

    Update Part 14

--- Part_14_Classes/0007_escape_room/main.py
+++ Part_14_Classes/0007_escape_room/main.py
@@ -44,74 +44,69 @@ if __name__ == '__main__':
         previous_player_location = player_location
         grid.display(np, GRID_HEIGHT, GRID_WIDTH, LED_COUNT, update_grid)
 
-        print(btn_1.value())
-        print(btn_2.value())
-        print(btn_3.value())
-        print(btn_4.value())
+        while True:
+            # noinspection PyArgumentList
+            if not btn_1.value():
+                player_location = player.move_east(grid)
+                update_grid = grid.update(player)
+                break
+            elif not btn_2.value():
+                player_location = player.move_west(grid)
+                update_grid = grid.update(player)
+                break
+            elif not btn_3.value():
+                player_location = player.move_north(grid)
+                update_grid = grid.update(player)
+                break
+            elif not btn_4.value():
+                player_location = player.move_south(grid)
+                update_grid = grid.update(player)
+                break
 
-        input()
-        # while True:
-        #     # noinspection PyArgumentList
-        #     if btn_1.value():
-        #         player_location = player.move_east(grid)
-        #         update_grid = grid.update(player)
-        #         break
-        #     elif btn_2.value():
-        #         player_location = player.move_west(grid)
-        #         update_grid = grid.update(player)
-        #         break
-        #     elif btn_3.value():
-        #         player_location = player.move_north(grid)
-        #         update_grid = grid.update(player)
-        #         break
-        #     elif btn_4.value():
-        #         player_location = player.move_south(grid)
-        #         update_grid = grid.update(player)
-        #         break
-        #
-        # if generate_random_location:
-        #     random_location = (x, y) = game.generate_random_numbers(grid)
-        #     print(random_location)
-        #     generate_random_location = False
-        #
-        # if random_location == player_location and random_location != previous_player_location:
-        #     random_question, answer_1, answer_2, answer_3, correct_answer_index, correct_answer \
-        #         = game.ask_random_question()
-        #     display.text(oled, random_question)
-        #     oled.show()
-        #     sleep(5)
-        #     oled.fill(0)
-        #     oled.show()
-        #     display.texts(oled, answer_1, answer_2, answer_3)
-        #     oled.show()
-        #     while True:
-        #         # noinspection PyArgumentList
-        #         if btn_1.value():
-        #             response = 1
-        #             break
-        #         elif btn_2.value():
-        #             response = 2
-        #             break
-        #         elif btn_3.value():
-        #             response = 3
-        #             break
-        #     if response == correct_answer_index + 1:
-        #         display.text(oled, game.correct_answer_response())
-        #         inventory = player.get_inventory(file_manager)
-        #         player.inventory.append(inventory)
-        #         if 'Red Key' in player.inventory:
-        #             final_question = True
-        #         if 'Red Key' not in player.inventory and not final_question:
-        #             receive_red_key = game.generate_random_number(grid)
-        #             if receive_red_key == 2:
-        #                 display.text(oled, player.pick_up_red_key(file_manager))
-        #                 final_question = True
-        #             else:
-        #                 display.text(oled, player.without_red_key())
-        #         elif final_question:
-        #             display.text(oled, game.win(file_manager))
-        #             # music.play(music.POWER_UP)
-        #             # display.show(Image.ALL_CLOCKS, loop=True, delay=100)
-        #     else:
-        #         display.text(oled, game.incorrect_answer_response(correct_answer))
-        #     generate_random_location = True
+        if generate_random_location:
+            random_location = (x, y) = game.generate_random_numbers(grid)
+            print(random_location)
+            generate_random_location = False
+
+        if random_location == player_location and random_location != previous_player_location:
+            grid.display(np, GRID_HEIGHT, GRID_WIDTH, LED_COUNT, update_grid)
+            random_question, answer_1, answer_2, answer_3, correct_answer_index, correct_answer \
+                = game.ask_random_question()
+            display.text(oled, random_question)
+            oled.show()
+            sleep(5)
+            oled.fill(0)
+            oled.show()
+            display.texts(oled, answer_1, answer_2, answer_3)
+            oled.show()
+            while True:
+                # noinspection PyArgumentList
+                if btn_1.value():
+                    response = 1
+                    break
+                elif btn_2.value():
+                    response = 2
+                    break
+                elif btn_3.value():
+                    response = 3
+                    break
+            if response == correct_answer_index + 1:
+                display.text(oled, game.correct_answer_response())
+                inventory = player.get_inventory(file_manager)
+                player.inventory.append(inventory)
+                if 'Red Key' in player.inventory:
+                    final_question = True
+                if 'Red Key' not in player.inventory and not final_question:
+                    receive_red_key = game.generate_random_number(grid)
+                    if receive_red_key == 2:
+                        display.text(oled, player.pick_up_red_key(file_manager))
+                        final_question = True
+                    else:
+                        display.text(oled, player.without_red_key())
+                elif final_question:
+                    display.text(oled, game.win(file_manager))
+                    # music.play(music.POWER_UP)
+                    # display.show(Image.ALL_CLOCKS, loop=True, delay=100)
+            else:
+                display.text(oled, game.incorrect_answer_response(correct_answer))
+            generate_random_location = True
